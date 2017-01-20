d7129f47fa50e80a7c08c772e72fae0c247ee5ef
Tatiane Troxell <tatianetroxell@gmail.com>
2017-01-20 14:50:12 -0500

    first commit

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,6 +10,43 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Virtual Pet:");
+            Console.WriteLine("This is your new Dog. Please give it a name?");
+            string petName = Console.ReadLine();
+
+            VirtualPet dog = new VirtualPet(petName, 20, 12, 8);
+
+            Console.WriteLine("Dog Name: " + petName);
+            Console.WriteLine("Hunger: " + dog.Hunger);
+            Console.WriteLine("Thirst: " + dog.Thirst);
+            Console.WriteLine("Tiredness: " + dog.Tiredness);
+            Console.WriteLine();
+
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1 - Feed the dog");
+            Console.WriteLine("2 - Give water to the dog");
+            Console.WriteLine("3 - Put dog to sleep");
+            Console.WriteLine("4 - Nothing");
+            int userInput = int.Parse(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 1:
+                    dog.FeedPet();
+                    Console.WriteLine("You feed " + petName);
+                    break;
+                case 2:
+                    dog.GiveWater();
+                    Console.WriteLine("You gave water to " + petName);
+                    break;
+                case 3:
+                    dog.PutPetToSleep();
+                    Console.WriteLine("You put " + petName + " to sleep");
+                    break;
+                default:
+                    Console.WriteLine("You didn't do anything ");
+                    break;
+            }
 
         }
     }
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        private string petName;
+        private int hunger;
+        private int thirst;
+        private int tiredness;
+
+        //Properties
+        public string PetName
+        {
+            get { return this.petName; }
+            set { this.petName = value; }
+        }
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+
+        public int Tiredness
+        {
+            get { return this.tiredness; }
+            set { this.tiredness = value; }
+        }
+
+        //Constructors
+        public VirtualPet(string petName, int hunger, int thirst, int tiredness)
+        {
+            this.petName = petName;
+            this.hunger = hunger;
+            this.thirst = thirst;
+            this.tiredness = tiredness;
+        }
+
+        //Methods
+        public void FeedPet()
+        {
+            hunger -= 5;
+        }
+
+        public void GiveWater()
+        {
+            thirst -= 5;
+        }
+
+        public void PutPetToSleep()
+        {
+            tiredness -= 5;
+        }
+
+
+    }
+}
