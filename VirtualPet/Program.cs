1661a5850757e3f18dfb9b132acfafc2ce8d899a
Tatiane Troxell <tatianetroxell@gmail.com>
2017-01-22 21:35:09 -0500

    First Commit

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -11,43 +11,65 @@ namespace VirtualPet
         static void Main(string[] args)
         {
             Console.WriteLine("Virtual Pet:");
-            Console.WriteLine("This is your new Dog. Please give it a name?");
-            string petName = Console.ReadLine();
+            Console.WriteLine("      __");
+            Console.WriteLine(" (___()'`;");
+            Console.WriteLine(" /,    /`");
+            Console.WriteLine(" \\\\---\\\\");
+            Console.WriteLine("This is your new dog. Please give it a name?");
+            string petName = Console.ReadLine(); //Assign the value entered by the user to the variable petName
 
-            VirtualPet dog = new VirtualPet(petName, 20, 12, 8);
-
-            Console.WriteLine("Dog Name: " + petName);
-            Console.WriteLine("Hunger: " + dog.Hunger);
-            Console.WriteLine("Thirst: " + dog.Thirst);
-            Console.WriteLine("Tiredness: " + dog.Tiredness);
+            //Create an new object of VirtualPet, and assign its values(petName, hunger, thrist, tiredness, boredom
+            VirtualPet dog = new VirtualPet(petName, 20, 12, 18, 5);
+            Console.WriteLine();
+            Console.WriteLine("Do you want to interact with {0} (Y/N)?", petName);
+            string userAnswer = Console.ReadLine();
             Console.WriteLine();
 
-            Console.WriteLine("What do you want to do?");
-            Console.WriteLine("1 - Feed the dog");
-            Console.WriteLine("2 - Give water to the dog");
-            Console.WriteLine("3 - Put dog to sleep");
-            Console.WriteLine("4 - Nothing");
-            int userInput = int.Parse(Console.ReadLine());
-
-            switch (userInput)
+            //If the user wants to interact with the dog, the program will ask what the user wants to do
+            //If not the program will quit
+            do
             {
-                case 1:
-                    dog.FeedPet();
-                    Console.WriteLine("You feed " + petName);
-                    break;
-                case 2:
-                    dog.GiveWater();
-                    Console.WriteLine("You gave water to " + petName);
-                    break;
-                case 3:
-                    dog.PutPetToSleep();
-                    Console.WriteLine("You put " + petName + " to sleep");
-                    break;
-                default:
-                    Console.WriteLine("You didn't do anything ");
-                    break;
-            }
+                //Let the user know the status of the dog if any of the level are more than 15
+                //(ex: if the hunger level is more than 15, tell the user the dog is hungry
+                dog.PetStatus();
+                Console.WriteLine();
+                Console.WriteLine("What do you want to do? (Please enter a number)");
+                Console.WriteLine("1 - Feed {0}", petName);
+                Console.WriteLine("2 - Give water to {0}", petName);
+                Console.WriteLine("3 - Put {0} to sleep", petName);
+                Console.WriteLine("4 - Play with {0}", petName);
+                Console.WriteLine("5 - Do nothing with {0}", petName);
+                int userInput = int.Parse(Console.ReadLine());
 
+                //Call methods based on the user's answer and let them know what they did with the dog
+                switch (userInput)
+                {
+                    case 1:
+                        dog.FeedPet();
+                        break;
+                    case 2:
+                        dog.GiveWater();
+                        break;
+                    case 3:
+                        dog.PutPetToSleep();
+                        break;
+                    case 4:
+                        dog.PlayWithPet();
+                        break;
+                    default:
+                        Console.WriteLine("You didn't do anything ");
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Do you still want to interact with {0} (Y/N)?", petName);
+                userAnswer = Console.ReadLine();
+
+                Console.WriteLine();
+
+            } while (userAnswer.ToUpper() == "Y" || userAnswer.ToUpper() == "YES");
+
+            Console.WriteLine("Thank you for playing!!!");
         }
     }
 }
--- VirtualPet/VirtualPet.cs
+++ VirtualPet/VirtualPet.cs
@@ -12,6 +12,7 @@ namespace VirtualPet
         private int hunger;
         private int thirst;
         private int tiredness;
+        private int boredom;
 
         //Properties
         public string PetName
@@ -38,31 +39,72 @@ namespace VirtualPet
             set { this.tiredness = value; }
         }
 
+        public int Boredom
+        {
+            get { return this.boredom; }
+            set { this.boredom = value; }
+        }
+
         //Constructors
-        public VirtualPet(string petName, int hunger, int thirst, int tiredness)
+        public VirtualPet(string petName, int hunger, int thirst, int tiredness, int boredom)
         {
             this.petName = petName;
             this.hunger = hunger;
             this.thirst = thirst;
             this.tiredness = tiredness;
+            this.boredom = boredom;
         }
 
         //Methods
         public void FeedPet()
         {
             hunger -= 5;
+            thirst += 5;
+            Console.WriteLine("You fed {0}.", petName);
         }
 
         public void GiveWater()
         {
             thirst -= 5;
+            Console.WriteLine("You gave water to {0}.", petName);
         }
 
         public void PutPetToSleep()
         {
             tiredness -= 5;
+            Console.WriteLine("You put {0} to sleep.", petName);
         }
 
+        public void PlayWithPet()
+        {
+            boredom -= 5;
+            tiredness += 5;
+            thirst += 5;
+            Console.WriteLine("You played with {0}.", petName);
+        }
 
+        //Let the user know if the dog is hungry, thirsty, tired or bored when method is called
+        public void PetStatus()
+        {
+            if (hunger > 15)
+            {
+                Console.WriteLine("{0} is hungry", petName);
+            }
+
+            if (thirst > 15)
+            {
+                Console.WriteLine("{0} is thirsty", petName);
+            }
+
+            if (tiredness > 15)
+            {
+                Console.WriteLine("{0} is tired", petName);
+            }
+
+            if (boredom > 15)
+            {
+                Console.WriteLine("{0} is bored", petName);
+            }
+        }
     }
 }
